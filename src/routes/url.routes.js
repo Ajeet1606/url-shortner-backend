0aad595965417c6d96c0f3e3c1c39a9833e5f3c6
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2024-10-06 11:36:02 +0000

    test route udpated

--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { handleGenerateNewShortUrl, handleGetShortUrl, handleGetAnalytics, greetings } from "../controllers/url.controllers.js";
 const router = express.Router();
-router.get("/hello", greetings);
+router.get("/", greetings);
 router.post("/shortUrl", handleGenerateNewShortUrl);
 router.get("/:shortId", handleGetShortUrl);
 router.get("/analytics/:shortId", handleGetAnalytics);
